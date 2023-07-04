547fcd24615ef19ab6269dff496254237a380893
Omar Arias <72229639+omararias@users.noreply.github.com>
2023-07-04 17:21:36 -0600

    Add files via upload
    Agregada clase encriptacion, con encriptacion MD5, cambios en CSV al guardar contrasenas cifradas y no mostrar contrasena al escribirla

---
+++ construccion_refactoring-refactoring/Proyecto_Final_POO/src/Modelo/Persona.java
@@ -0,0 +1,50 @@
+
+package Modelo;
+
+import java.io.Serializable;
+
+
+
+
+public abstract class Persona implements Serializable {
+    // Atributos
+    public String nombre;
+    public String apellidoPaterno;
+    public String apellidoMaterno;
+
+    // Constructor
+    public Persona(String nombre, String apellidoPaterno, String apellidoMaterno){
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    // Metodo toString
+    @Override
+    public String toString() {
+        return "Nombre: " + getNombre() + " " + getApellidoPaterno() + " " + getApellidoMaterno();
+    }
+
+}
